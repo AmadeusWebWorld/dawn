5c861c82d8a14f45e923fdae640369a8f909ed31
Imran Ali Namazi <imran@amadeusweb.com>
2025-08-24 17:14:38 +0530

    repositories revamp

--- admin/git-manager/tools/refresh-repo-list.php
+++ admin/git-manager/tools/refresh-repo-list.php
@@ -7,6 +7,10 @@ echo '<h1>Searching In Repos:</h1>';
 $sheet = getSheet(NODEPATH . '/view/git-accounts.tsv', false);
 $sources = [];
 
+DEFINE('CARDTEMPLATE', '<span class="float-right">%owner_link_md%</span>|%repo_link_md% &mdash;> %website_link_md%|%description%|');
+DEFINE('GITHUBORGLINK',  '[%name%](https://github.com/orgs/%name%/repositories)');
+DEFINE('GITHUBUSERLINK', '[%name%](https://github.com/%name%?tab=repositories)');
+
 foreach ($sheet->rows as $item) {
 	$item = rowToObject($item, $sheet);
 
@@ -17,11 +21,9 @@ foreach ($sheet->rows as $item) {
 
 	$sources[] = $repos;
 	echo 'Found: ' . count($repos) . ' repos with names:';
-	echo implode(NEWLINE, array_map(function ($repo) { return BRNL . ' &bull; ' . getLink($repo['repo_name'], $repo['repo_url'], '', true) . ' &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ' . 
-		($repo['website'] ? getLink($repo['website'], $repo['website'], '', true) : '(empty)' ). BRNL . '<i>' . $repo['description'] . '</i>' ; }, $repos));
+	echo implode(NEWLINE . '<hr style="margin: 6px" />', array_map(function ($repo) { return BRNL . returnLine(pipeToBR(replaceItems(CARDTEMPLATE, $repo, '%'))); }, $repos));
 
-	//break;
-	echo '<hr />' . NEWLINE;
+	echo cbCloseAndOpen('container') . NEWLINE;
 }
 
 $op = [];
@@ -35,8 +37,8 @@ foreach ($sources as $repos) {
 	}
 }
 $op[] = '';
-file_put_contents(__DIR__ . '/repo-list.tsv', implode(NEWLINE, $op));
-echo 'Written ' . (count($op) - 2) . ' lines to ' . getLink('repo list', '../repo-list/');
+file_put_contents(__DIR__ . '/../view/repositories.tsv', implode(NEWLINE, $op));
+echo 'Written ' . (count($op) - 2) . ' lines to ' . getLink('repo list', '../../view/repositories/');
 
 contentBox('end');
 
@@ -55,24 +57,23 @@ function _gitHubToOurs($url) {
 	foreach ($raw as $item) {
 		if ($skip && !$item['homepage']) continue;
 
+		$owner = $item['owner'];
+		$org = $owner['type'] == 'Organization';
+		$ownerLink = replaceItems($org ? GITHUBORGLINK : GITHUBUSERLINK, [ 'name' => $owner['login'] ], '%');
+
 		$op[$item['name']] = [
-			'key' => $item['full_name'],
+			//'key' => $item['full_name'],
 			'id' => $item['id'],
 
-			'owner' => $item['owner']['login'],
-			'owner_url' => $item['owner']['html_url'],
-			'owner_type' => $item['owner']['type'],
-
-			'repo_name' => $item['name'],
-			'repo_url' => $item['html_url'],
-			'repo_git_url' => $item['git_url'],
+			'owner_link_md' => $ownerLink,
+			'repo_link_md' => '[' . $item['name'] . '](' . $item['html_url'] . ')',
 
 			'description' => $item['description'],
 
+			'website_link_md' => !!$item['homepage'] ? '--empty--' : '[' . $item['homepage'] . '](' . $item['homepage'] . ')',
+
 			'created' => $item['created_at'],
 			'updated' => $item['updated_at'],
-
-			'website' => $item['homepage'],
 		];
 	}
 
