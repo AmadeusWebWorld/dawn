ab19f7fe9ebdb309ceb681434f52769f06e6a0a3
Imran @ JE <imran@amadeusweb.world>
2025-09-17 13:19:43 +0530

    more testing needed

--- core/data/core-code-snippets/media.php
+++ core/data/core-code-snippets/media.php
@@ -10,24 +10,24 @@ $allItems = $sheet->rows;
 
 if (isset($sheet->columns['site']) && $thisHome = variable('subsiteHome')) {
 	$thisName = $thisHome['Path'];
-
 	$bySite = arrayGroupBy($sheet->rows, $sheet->columns['site']);
 	$bySiteAndName = valueIfSet($bySite, $thisName, []);
-	$bySiteAndAll = count($bySiteAndName) ? [] : valueIfSet($bySite, '*', []);
+	$bySiteAndAll = valueIfSet($bySite, '*', []);
 	$allItems = array_merge($bySiteAndName, $bySiteAndAll);
 }
 
 if (count($allItems)) {
 	$byNode = arrayGroupBy($allItems, $sheet->columns['node']);
 	$byNodeAndName = valueIfSet($byNode, nodeValue(), []);
-	$byNodeAndAll = count($byNodeAndName) ? [] : valueIfSet($byNode, '*', []);
+	$byNodeAndAll = valueIfSet($byNode, '*', []);
 	$allItems = array_merge($byNodeAndName, $byNodeAndAll);
+	if (!count($allItems)) $allItems = $bySiteAndAll;
 }
 
 if (!count($allItems)) return !variable('local') ? ''
 	: h2('No items resolved for current page.', 'text-danger', true) . '<p>Please check sufficient data in the "data/media.tsv" file.</p>';
 
-$items = getShuffledItems($allItems, 2);
+$items = getShuffledItems($allItems, 1);
 $html = '';
 foreach ($items as $item)
 	$html .= replaceItems('[%type%]%id%[/%type%]' . NEWLINES2, $sheet->asObject($item) , '%');
